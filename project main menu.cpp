f797073ebbea50bded41b246efdcbd4d5bdf64cb
divs3 <divss2020@gmail.com>
2026-06-08 19:33:55 +0545

    change in programming

--- project main menu.cpp
+++ project main menu.cpp
@@ -1,14 +1,14 @@
  /*
     ROCK PAPER SCISSORS - SEMESTER PROJECT
-    Part 1: Main Menu Only + login + register
+    Part 1: Main Menu Only + login + register + Gameplay
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <string.h>
-#include <time.h>
-#include <ctype.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cctype>
 
 // ----------CONFIGURATION-----------
 #define MAX_USERS 100
@@ -41,6 +41,12 @@ void mainMenu(void);
 void login(void);
 void registerUser(void);
 int findUser(char *username);
+void addHistory(int idx, char *result);
+void saveData(void);
+void gameDashboard(void);
+void playSinglePlayer(void);
+void playMultiplayer(void);
+
 // ==========================================
 // HELPER FUNCTIONS
 // ==========================================
@@ -58,6 +64,28 @@ int findUser(char *username) {
 void clearScreen(void) {
     system("cls");
 }
+void addHistory(int idx, char *result) {
+    char temp[MAX_HISTORY_LEN + 100];
+    sprintf(temp, "%s | %s", result, users[idx].history);
+    strncpy(users[idx].history, temp, MAX_HISTORY_LEN - 1);
+    users[idx].history[MAX_HISTORY_LEN - 1] = '\0';
+}
+void saveData(void) {
+    FILE *fp;
+    fp = fopen(FILE_NAME, "wb");
+    if (fp != NULL) {
+        fwrite(&userCount, sizeof(int), 1, fp);
+        fwrite(users, sizeof(User), userCount, fp);
+        fclose(fp);
+    }
+}
+
+
+
+
+
+
+
 // ==========================================
 // REGISTER FUNCTION
 // ==========================================
@@ -153,19 +181,85 @@ void login(void) {
     getch();
 }
 
+// ==========================================
+//SINGLE PLAYER GAME
+//===========================================
+
+void playSinglePlayer(void) {
+    int playerChoice, compChoice;
+    char *choices[] = {"Rock", "Paper", "Scissors"};
+    
+ 
+    srand(time(NULL));
+
+    clearScreen();
+    printf("=================================\n");
+    printf("       SINGLE PLAYER MODE\n");
+    printf("=================================\n");
+    printf("1. Rock\n");
+    printf("2. Paper\n");
+    printf("3. Scissors\n");
+    printf("=================================\n");
+    printf("Enter your choice (1-3): ");
+
+    scanf("%d", &playerChoice);
+    getchar();
+
+    if (playerChoice < 1 || playerChoice > 3) {
+        printf("\nInvalid choice! Turn forfeited.\n");
+        getch();
+        return;
+    }
+
+    compChoice = (rand() % 3) + 1;
+
+    printf("\nYOU chose:     %s\n", choices[playerChoice - 1]);
+    printf("COMP chose:    %s\n", choices[compChoice - 1]);
+
+    int result = 0;
+
+    if (playerChoice == compChoice) {
+        printf("\n>>> IT'S A DRAW! <<<\n");
+        result = 0;
+    } else if ((playerChoice == 1 && compChoice == 3) ||
+               (playerChoice == 2 && compChoice == 1) ||
+               (playerChoice == 3 && compChoice == 2)) {
+        printf("\n>>> YOU WIN! <<<\n");
+        result = 1;
+    } else {
+        printf("\n>>> YOU LOSE! <<<\n");
+        result = -1;
+    }
+
+    if (result == 1) {
+        users[currentUserIndex].wins++;
+    } else if (result == 0) {
+        users[currentUserIndex].draws++;
+    } else {
+        users[currentUserIndex].losses++;
+    }
+
+    char logEntry[100];
+    sprintf(logEntry, "Solo: %s vs %s [%c]", 
+            choices[playerChoice - 1], 
+            choices[compChoice - 1],
+            result == 1 ? 'W' : (result == -1 ? 'L' : 'D'));
+    
+    addHistory(currentUserIndex, logEntry);
+    saveData();
+
+    printf("\nPress any key to continue...");
+    getch();
+}
+
+// ==========================================
+// MULTIPLAYER GAME
+// ==========================================
 
 
-/* Add more prototypes here as we add modules:
-void login(void);
-void registerUser(void);
-void gameDashboard(void);
-void playGame(int mode);
-void showHistory(void);
-void showLeaderboard(void);
-void crudMenu(void);
-void saveDataToFile(void);
-void loadDataFromFile(void);
-*/
+
+
+
 
 /* ==========================================
    MAIN MENU
@@ -175,29 +269,14 @@ void loadDataFromFile(void);
 void mainMenu(void) {
     int choice;
     
-     while (1) {
-	 clearScreen();
-    printf("=================================\n");
-    printf("   ROCK PAPER SCISSORS - MAIN MENU\n");
-    printf("=================================\n");
-    
-    if (currentUserIndex == -1) {
-    printf("1. Login\n");
-    printf("2. Register\n");
-    printf("3. Exit\n");
-} else {
- printf("Logged in as: %s\n", users[currentUserIndex].username);
-            printf("1. Play Game\n");
-            printf("2. Logout\n");
-            printf("3. Exit\n");
-        }
-    printf("=================================\n");
-    printf("Enter choice: ");
-
-    scanf("%d", &choice);
-    getchar();
-    if (currentUserIndex == -1) {
-    printf("1. Login\n");
+    while (1) {
+        clearScreen();
+        printf("=================================\n");
+        printf("   ROCK PAPER SCISSORS - MAIN MENU\n");
+        printf("=================================\n");
+        
+        if (currentUserIndex == -1) {
+            printf("1. Login\n");
             printf("2. Register\n");
             printf("3. Exit\n");
         } else {
@@ -206,12 +285,17 @@ void mainMenu(void) {
             printf("2. Logout\n");
             printf("3. Exit\n");
         }
-printf("=================================\n");
+        printf("=================================\n");
+        printf("Enter choice: ");
+
+        scanf("%d", &choice);
+        getchar();
+
         if (currentUserIndex == -1) {
             if (choice == 1) {
                 login();
             } else if (choice == 2) {
-            	registerUser();
+                registerUser();
             } else if (choice == 3) {
                 printf("Thanks for playing!\n");
                 break;
@@ -219,7 +303,7 @@ printf("=================================\n");
                 printf("Invalid choice!\n");
                 getch();
             }
-            } else {
+        } else {
             if (choice == 1) {
                 printf("\n[Game module - coming soon]");
                 getch();
@@ -228,14 +312,14 @@ printf("=================================\n");
                 printf("Logged out!\n");
                 getch();
             } else if (choice == 3) {
-            	printf("Thanks for playing!\n");
+                printf("Thanks for playing!\n");
                 break;
             } else {
                 printf("Invalid choice!\n");
                 getch();
             }
         }
-		}
+    }
 }
 
 /* ==========================================
